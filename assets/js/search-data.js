0f8058d339b9fdbd7fc792530d70986dc4f5afc3
dododoner <83332760+dododoner@users.noreply.github.com>
2025-03-07 13:17:49 +0000

    Deploying to gh-pages from @ fabian-m-fuchs/fabian-m-fuchs.github.io@3b705a4e526af7dd33f826b31a727c855ab3e883 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,85 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "news-today-i-presented-my-upcoming-work-a-comparison-principle-based-on-couplings-of-partial-integro-differential-operators-at-the-fdm-seminar-of-universität-freiburg",
+          title: 'Today, I presented my upcoming work A comparison principle based on couplings of...',
+          description: "",
+          section: "News",},{id: "news-the-preprint-a-comparison-principle-based-on-couplings-of-partial-integro-differential-operators-with-serena-della-corte-richard-kraaij-and-max-nendel-is-online-now",
+          title: 'The preprint A comparison principle based on couplings of partial integro-differential operators with...',
+          description: "",
+          section: "News",},{id: "news-today-we-concluded-the-19th-bigsem-workshop-in-which-21-young-researchers-from-all-over-europe-and-north-america-presented-their-work-in-economics-management-and-finance-check-out-the-recap-here",
+          title: 'Today, we concluded the 19th BiGSEM Workshop, in which 21 young researchers from...',
+          description: "",
+          section: "News",},{id: "news-the-preprint-existence-of-viscosity-solutions-to-abstract-cauchy-problems-via-nonlinear-semigroups-with-max-nendel-is-online-now",
+          title: 'The preprint Existence of Viscosity Solutions to Abstract Cauchy Problems via Nonlinear Semigroups...',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%66%61%62%69%61%6E.%66%75%63%68%73@%75%6E%69-%62%69%65%6C%65%66%65%6C%64.%64%65", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/fabian-m-fuchs", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0006-2819-585X", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
